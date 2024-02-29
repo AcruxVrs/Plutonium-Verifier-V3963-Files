b0656b3666166ee22708bf620e07f53ee121d235
AcruxVrs <79591026+AcruxVrs@users.noreply.github.com>
2024-02-29 16:12:30 +0000

    update

--- filechecker.py
+++ filechecker.py
@@ -22,16 +22,35 @@ def verify_files(original_file, copied_file):
         print(f"File integrity verification failed: {original_file} and {copied_file} - contents do not match.")
         return False
 
-def check_files(main_folder, main_copy_folder):
+def check_files(main_folder, main_copy_folder, exclude_files=None, exclude_dirs=None):
     """Check files in the main folder and its subfolders."""
     total_replaced = 0
 
+    if exclude_dirs is None:
+        exclude_dirs = set()
+
+    if exclude_files is None:
+        exclude_files = set()
+
     # Iterate over files in the main copy folder
     for root, dirs, files in os.walk(main_copy_folder):
+        if any(d in exclude_dirs for d in dirs):
+            excluded_dir = [d for d in dirs if d in exclude_dirs][0]
+            dirs.remove(excluded_dir)
+            print(f"Excluded directory: {os.path.join(root, excluded_dir)}")
+            continue
+
         for copied_file in files:
             copied_path = os.path.join(root, copied_file)
             relative_path = os.path.relpath(copied_path, main_copy_folder)
             original_path = os.path.join(main_folder, relative_path)
+
+             # Check if the file should be excluded
+            if any(copied_file.startswith(exclude) for exclude in exclude_files):
+                # Check if the corresponding file exists in the main folder before excluding it
+                
+                print(f"Excluded file: {copied_path}")
+                continue
 
             # Create destination directory if it doesn't exist
             os.makedirs(os.path.dirname(original_path), exist_ok=True)
@@ -54,5 +73,7 @@ def check_files(main_folder, main_copy_folder):
 # Example usage:
 main_folder = os.path.join(os.environ['LOCALAPPDATA'], 'Plutonium')
 main_copy_folder = './PlutoniumCopy - Copy'
+exclude_dirs = {'players'}
+exclude_files = {'dvars'}
 
-check_files(main_folder, main_copy_folder)
+check_files(main_folder, main_copy_folder, exclude_files=exclude_files, exclude_dirs=exclude_dirs)
